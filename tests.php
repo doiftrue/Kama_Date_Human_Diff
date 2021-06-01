41ce273fa721a0d00ec0b34fb4c977ac4ce626c5
Kama <tkama@bk.ru>
2021-06-01 08:43:27 +0500

    Create tests.php

---
+++ tests.php
@@ -0,0 +1,39 @@
+$func = static function( $format, $from ){
+    return sprintf( "\t%s\n", Kama_Date_Human_Diff::human_diff( $format, $from ) );
+};
+
+
+$format = 'd m Y h';
+
+echo "\nFuture:\n";
+
+echo $func( $format, time() + 25 * 3600 );
+echo $func( $format, time() + 24 * 3600 );
+echo $func( $format, time() + ( 10 + 3600 ) );
+echo $func( $format, time() + 10 * 60 );
+echo $func( $format, time() + 65 );
+echo $func( $format, time() + 30 );
+echo "\nPast:\n";
+echo $func( $format, time() - 30 );
+echo $func( $format, time() - 65 );
+echo $func( $format, time() - 10 * 60 );
+echo $func( $format, time() - ( 10 + 3600 ) );
+echo $func( $format, time() - 24 * 3600 );
+echo $func( $format, time() - 25 * 3600 );
+
+
+$format = 'd m Y';
+
+echo "\nFuture:\n";
+echo $func( $format, time() + 99893605 );
+echo $func( $format, time() + 9893605 );
+echo $func( $format, time() + 893605 );
+echo $func( $format, time() + 83605 );
+echo $func( $format, time() + 23605 );
+echo "\nPast:\n";
+echo $func( $format, time() - 3605 );
+echo $func( $format, time() - 113605 );
+echo $func( $format, time() - 1113605 );
+echo $func( $format, time() - 11113605 );
+echo $func( $format, time() - 111113605 );
+echo $func( $format, time() - 1111113605 );
