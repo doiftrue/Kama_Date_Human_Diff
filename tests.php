4f2643fd2440c7626029360a2bb0b1fb7d4b473a
Kama <tkama@bk.ru>
2021-06-01 10:01:29 +0500

    Update tests.php

--- tests.php
+++ tests.php
@@ -6,13 +6,11 @@ $func = static function( $format, $from ){
 $format = 'd m Y h';
 
 echo "\nFuture:\n";
-
-echo $func( $format, time() + 25 * 3600 );
-echo $func( $format, time() + 24 * 3600 );
-echo $func( $format, time() + ( 10 + 3600 ) );
-echo $func( $format, time() + 10 * 60 );
-echo $func( $format, time() + 65 );
 echo $func( $format, time() + 30 );
+echo $func( $format, time() + 65 );
+echo $func( $format, time() + ( 10 + 3600 ) );
+echo $func( $format, time() + 24 * 3600 );
+echo $func( $format, time() + 25 * 3600 );
 echo "\nPast:\n";
 echo $func( $format, time() - 30 );
 echo $func( $format, time() - 65 );
@@ -25,11 +23,11 @@ echo $func( $format, time() - 25 * 3600 );
 $format = 'd m Y';
 
 echo "\nFuture:\n";
-echo $func( $format, time() + 99893605 );
-echo $func( $format, time() + 9893605 );
-echo $func( $format, time() + 893605 );
-echo $func( $format, time() + 83605 );
 echo $func( $format, time() + 23605 );
+echo $func( $format, time() + 83605 );
+echo $func( $format, time() + 893605 );
+echo $func( $format, time() + 9893605 );
+echo $func( $format, time() + 99893605 );
 echo "\nPast:\n";
 echo $func( $format, time() - 3605 );
 echo $func( $format, time() - 113605 );
